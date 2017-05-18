86b473bf8585febd9d32ba262f024feec1b4875c
guohao <510485959@qq.com>
2017-05-18 13:31:56 +0800

    手机端项目上传

---
+++ 练习wx/js/app/app1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+var app1=angular.module("myApp1",["ionic"]);
+app1.controller("myCtrl",["$scope",function ($scope) {
+
+}])
+
+
---
+++ 练习wx/js/app/app2.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+var app2=angular.module("myApp2",["ionic"]);
+app2.controller("myCtrl",["$scope",function ($scope) {
+}])
